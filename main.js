fe0bfeb9a294081a2fb4006d86b03e571c0a907f
Joseph Woodley <josephjwoodley@gmail.com>
2019-04-13 19:43:49 -0400

    Create JSON, move Javascript to .js

---
+++ main.js
@@ -0,0 +1,33 @@
+function myFunction()
+                    {
+                        var x;
+                        var input_item = prompt("Please enter your name Grocery item");
+        
+                        if (input_item!=null)
+                        {
+                            x = "Grocery item: " + input_item;
+                            document.getElementById("demo").innerHTML = x;
+                        }
+                    }
+
+                    var xhttp = new XMLHttpRequest();
+                        xhttp.onreadystatechange = function() {
+                            if (this.readyState == 4 && this.status == 200) {
+                                var response = JSON.parse(xhttp.responseText);
+                                console.log(response.grocery_items);
+                                console.log(response.grocery_items[0].aisle_location);                                
+                                console.log(response.grocery_items[1]);
+
+                                // var output = ' ';
+                                // for (var i = 0; i < grocery_items.length; i++) {
+                                //     if input_item === grocery_items.name[i] {
+                                //         console.log(grocery_items[i].aisle_location);
+                                //     }
+
+                                //     output += '<li>' + grocery_items[i].aisle_location + '</li';
+                                // }
+                                // document.getElementById('grocery_items').innerHTML = output;
+                            }
+                        };
+                        xhttp.open("GET", "items.json", true);
+                        xhttp.send();
