738486e3f9042e168bbe5866669d47645cbc5311
Joseph Woodley <josephjwoodley@gmail.com>
2019-04-14 23:45:34 -0400

    Display Aisle on page, Add to JSON file

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
+document.getElementById('theButton').addEventListener('click', myFunction)
+
+
 function myFunction() {
-    var x;
-    var input_item = prompt("Please enter your name Grocery item");
+    let item = document.getElementById("item").value
+    // console.log(item);
 
-    if (input_item != null) {
-        x = "Grocery item: " + input_item;
-        document.getElementById("demo").innerHTML = x;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        // console.log(item);
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(xhttp.responseText);
+            for (var i = 0; i < response.grocery_items.length; i++) {
+                if ((response.grocery_items[i].name) == item) {
+                    var results = (response.grocery_items[i].aisle_location);
+                    console.log(results);
+                    document.getElementById('myList').innerHTML = results;
+                }
+            }
+        }
     }
+    xhttp.open("GET", "items.json", true);
+    xhttp.send();
 }
-
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        var response = JSON.parse(xhttp.responseText);
-        console.log(response.grocery_items);
-        console.log(response.grocery_items[0].aisle_location);
-        console.log(response.grocery_items[1]);
-    }
-    for (var i = 0; i < response.grocery_items.length; i++) {
-        console.log(response.grocery_items[i].aisle_location);
-    }
-}
-
-xhttp.open("GET", "items.json", true);
-xhttp.send();
