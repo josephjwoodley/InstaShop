c18319936e98afe6aba582ca52f96753218f19c2
Joseph Woodley <josephjwoodley@gmail.com>
2019-04-15 19:54:58 -0400

    Added dropdown

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
-document.getElementById('theButton').addEventListener('click', myFunction)
+window.onload = populateSelect();
 
+function populateSelect() {
+
+    // CREATE AN XMLHttpRequest OBJECT, WITH GET METHOD.
+    var xhr = new XMLHttpRequest(),
+        method = 'GET',
+        overrideMimeType = 'application/json',
+        url = "items.json";        // ADD THE URL OF THE FILE.
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+
+            // PARSE JSON DATA.
+            var theList = JSON.parse(xhr.responseText);
+
+            var ele = document.getElementById('theSelect');
+            for (var i = 0; i < theList.grocery_items.length; i++) {
+                // BIND DATA TO <select> ELEMENT.
+                ele.innerHTML = ele.innerHTML +
+                    '<option value>' + theList.grocery_items[i].name + '</option>';
+            }
+        }
+    };
+    xhr.open(method, url, true);
+    xhr.send();
+}
+
+function show(ele) {
+    // GET THE SELECTED VALUE FROM <select> ELEMENT AND SHOW IT.
+    var msg = document.getElementById('msg');
+    msg.innerHTML = 'Selected Item: <b>' + ele.options[ele.selectedIndex].text;
+}
+
+document.getElementById('theButton').addEventListener('click', myFunction)
 
 function myFunction() {
     let item = document.getElementById("item").value
